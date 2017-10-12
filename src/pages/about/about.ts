88f42bce79f5443bb3b5924fe3a66aa9f9fbd991
Łukasz Olejarczuk <ol3j@o2.pl>
2017-10-13 01:03:56 +0200

    add camera

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Camera, CameraOptions } from '@ionic-native/camera';
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  public photo: string;
+
+  constructor(public navCtrl: NavController, private camera: Camera) {
 
   }
 
+  public takePhoto(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.photo = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      // Handle error
+     });
+  }
 }
